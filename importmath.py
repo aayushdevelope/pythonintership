f324c5b654d711b4789d7a281f4bac0419b7af10
aayushdevelope <aayushtiwari.0023@gmail.com>
2026-04-01 16:09:31 +0530

    Add files via upload

---
+++ importmath.py
@@ -0,0 +1,18 @@
+# In this program we will import math function which mean a math library with is use to perform 
+#some mathematic opoeration 
+
+import math
+a,b,c,d,e,f= 20.1,20.2,20.3,20.4,20.5,20.6
+
+print(math.floor(e))
+print(math.ceil(a))
+
+# In this program the floor value means the lower  actual value mean 20.5 the floor value is 20
+# In this program the ceil value means the upper actual value mean 20.5 the ceil value is 21
+
+print(round(b))
+print(round(f))
+
+''' IN ABOVE ROUND CODE THE ROUND VALUE OF B IS 20 AND THE ROUND VALUE OF F IS 21
+THIS IS BECAUSE THE VALUE FROM 20.1 TO 20.5 AFTER ROUNDING WILL CONVERTED AS FLOOR VALUE
+AND VALUE 20.6 TO 20.9 WILL CONVERTED AS CEIL VALUE
